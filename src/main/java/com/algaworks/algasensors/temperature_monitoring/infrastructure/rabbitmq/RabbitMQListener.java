18f1109f78d191db9e996b547fde1518f4129144
MatheusThierri <matheusthierri7@gmail.com>
2026-04-27 22:35:59 -0300

    Implement competing consumers pattern.

--- src/main/java/com/algaworks/algasensors/temperature_monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/algaworks/algasensors/temperature_monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -19,7 +19,7 @@ import static com.algaworks.algasensors.temperature_monitoring.infrastructure.ra
 public class RabbitMQListener {
     private final TemperatureMonitoringService temperatureMonitoringService;
 
-    @RabbitListener(queues = QUEUE)
+    @RabbitListener(queues = QUEUE, concurrency = "2-3")
     @SneakyThrows
     public void handle(@Payload TemperatureLogData temperatureLogData) {
         temperatureMonitoringService.processTemperatureReading(temperatureLogData);
